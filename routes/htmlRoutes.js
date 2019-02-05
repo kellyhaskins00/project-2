7f3868956837fdb004d475883ffbbd69e6c4d643
Kelly Haskins <kellyhaskins00@gmail.com>
2019-02-04 20:06:26 -0800

    updated files need to update map.handlebars file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,24 +11,30 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/views/layouts/map.handlebars", function (req, res) {
-    console.log("i am here!");
-    res.render("map", {
-      msg: "Welcome!"
-    });
-  });
-
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
+    // Load map page
+    app.get("/views/layouts/map.handlebars", function (req, res) {
+      console.log("i am here!");
+      res.render("map", {
+        msg: "Welcome!"
+      });
+    });// Load game page
+    app.get("/views/views/layouts/game.handlebars", function (req, res) {
+      console.log("this is the game!");
+      res.render("game", {
+        msg: "Welcome!"
+      });
+    });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
