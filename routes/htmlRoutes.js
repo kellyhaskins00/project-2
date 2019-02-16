a1677e0996df3a72b74fd0ad0b733c59d59afdc1
Kelly Haskins <kellyhaskins00@gmail.com>
2019-02-16 15:19:28 -0800

    added options and cute logos

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -19,14 +19,39 @@ module.exports = function(app) {
       });
     });
   });
-    // Load game page
-    // app.get("/views/layouts/game.handlebars", function (req, res) {
-    //   console.log("i am here!");
-    //   res.render("game", {
-    //     msg: "Welcome!"
-    //   });
-    // });
-  
+
+  // Load beer page
+  app.get("/views/index.handlebars", function (req, res) {
+    console.log("i am here!");
+    res.render("index", {
+      // msg: "Welcome!"
+    });
+  });
+
+    // Load wine page
+  app.get("/views/wine.handlebars", function (req, res) {
+    console.log("i am here!");
+    res.render("wine", {
+      // msg: "Welcome!"
+    });
+  });
+
+  // Load coffee page
+  app.get("/views/coffee.handlebars", function (req, res) {
+    console.log("i am here!");
+    res.render("coffee", {
+      // msg: "Welcome!"
+    });
+  });
+
+    // Load date page
+    app.get("/views/date.handlebars", function (req, res) {
+      console.log("i am here!");
+      res.render("date", {
+        // msg: "Welcome!"
+      });
+    });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
